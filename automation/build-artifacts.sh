4488db17feca95b9129e04e72dc3a473a0abc25d
Martin Perina <mperina@redhat.com>
2017-08-15 09:54:39 +0200

    Adding WildFly 8.2.1 package
    Change-Id: I42b860fb1ddcf112fcbf533789d62be5e045be43
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ automation/build-artifacts.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -e
+
+# WildFly version specification
+version="8.2.1"
+qualifier="Final"
+
+
+# The name and source of the package
+name="ovirt-engine-wildfly"
+src="wildfly-${version}.${qualifier}.zip"
+url="http://download.jboss.org/wildfly/${version}.${qualifier}/${src}"
+
+# Download the source:
+if [ ! -f "${src}" ]
+then
+    wget -O "${src}" "${url}"
+fi
+
+# Generate the spec from the template:
+sed \
+    -e "s/@VERSION@/${version}/g" \
+    -e "s/@QUALIFIER@/${qualifier}/g" \
+    -e "s/@SRC@/${src}/g" \
+    < "${name}.spec.in" \
+    > "${name}.spec"
+
+# Build the source and binary packages:
+rpmbuild \
+    -bs \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "${name}.spec"
+
+# Install any build requirements
+yum-builddep *src.rpm
+
+# Build RPMs
+rpmbuild \
+    -D "_rpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    --rebuild *.src.rpm
+
+# Store any relevant artifacts in exported-artifacts for the ci system to
+# archive
+[[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+find . -iname \*rpm -exec mv "{}" exported-artifacts/ \;
