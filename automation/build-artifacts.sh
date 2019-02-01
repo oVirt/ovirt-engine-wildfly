6b1d571f9afa2aaed7d1e7083340cd567b3a2d7c
Martin Perina <mperina@redhat.com>
2019-02-01 07:50:01 +0100

    core: Upgrade to WildFly 15.0.1.FINAL
    Change-Id: Ied09548cb1c5c31eb9d1864a3f0e2d48212104d3
    Bug-Url: https://bugzilla.redhat.com/1671635
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 
 # WildFly version specification
-WF_VERSION="14.0.1"
+WF_VERSION="15.0.1"
 WF_QUALIFIER="Final"
 
 # RPM version specification
 RPM_VERSION="${WF_VERSION}"
-RPM_RELEASE="3"
+RPM_RELEASE="1"
 
 export WF_VERSION WF_QUALIFIER RPM_VERSION RPM_RELEASE
 
